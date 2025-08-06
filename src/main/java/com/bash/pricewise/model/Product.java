079abb8cfad34097b27ec44e1c950da8c0872d8e
Bashir Muhammed <bashmufol@gmail.com>
2025-08-06 14:32:29 +0100

    updated

--- src/main/java/com/bash/pricewise/model/Product.java
+++ src/main/java/com/bash/pricewise/model/Product.java
@@ -9,7 +9,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@Table(name = "products")
+@Table(name = "product")
 public class Product {
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
